38c04a76a5c9b1904f0c48cb8a755240f91819d1
Tushar Jaiswal <54629298+tusharjaiswal123@users.noreply.github.com>
2020-02-21 17:54:28 +0530

    Create Lowest Common Ancestor in a Binary Tree.cpp

---
+++ Lowest Common Ancestor in a Binary Tree.cpp
@@ -0,0 +1,22 @@
+Node* lca(Node* root ,int n1 ,int n2 )
+{
+   if(root==NULL)
+   return NULL;
+   
+   if(root->data==n1 || root->data==n2)
+   return root;
+   
+   Node *left=lca(root->left,n1,n2);
+   Node *right=lca(root->right,n1,n2);
+   
+   if(left!=NULL && right!=NULL)
+   return root;
+   
+   else
+   {
+       if(left!=NULL)
+       return left;
+       else
+       return right;
+   }
+}
