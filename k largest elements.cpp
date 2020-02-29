2106f93097cbf9c4dfacc9c44fceb91ac9fe3d75
Tushar Jaiswal <54629298+tusharjaiswal123@users.noreply.github.com>
2020-02-29 19:20:45 +0530

    Update k largest elements.cpp

--- k largest elements.cpp
+++ k largest elements.cpp
@@ -9,21 +9,36 @@ int main() {
 	{
 	    int n,k,i,a;
 	    cin>>n>>k;
-	    
-	    int a[n];
-	    priority_queue<int> q;
+	  
+	    priority_queue<int,vector<int>,greater<int>> q;
 	    
 	    for(i=0;i<n;i++)
 	    {
 	        cin>>a;
+	        
+	        if(i<k)
 	        q.push(a);
+	        else
+	        {
+	            if(q.top()<a)
+	            {
+	                q.pop();
+	                q.push(a);
+	            }
+	        }
 	    }
 	    
-	    for(i=0;i<k;i++)
+	    vector<int> v;
+	    while(!q.empty())
 	    {
-	        cout<<q.top()<<" ";
+	        v.emplace_back(q.top());
 	        q.pop();
 	    }
+	    
+	    
+	    for(int i=v.size()-1;i>=0;i--)
+	    cout<<v[i]<<" ";
+	    
 	    cout<<endl;
 	}
 	return 0;
