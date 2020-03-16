c46657befa19f661086d41f19493e3ccc9b9df5c
Tushar Jaiswal <54629298+tusharjaiswal123@users.noreply.github.com>
2020-03-16 21:43:03 +0530

    Update Detect cycle in an undirected graph.cpp

--- Detect cycle in an undirected graph.cpp
+++ Detect cycle in an undirected graph.cpp
@@ -43,3 +43,36 @@ bool isCyclic(vector<int> adj[], int V)
 
 ## USING DFS
 
+bool isCyclicConn(vector<int> adj[],vector<bool>& vis,int s,int parent)
+{
+    vis[s]=true;
+    
+    for(auto v:adj[s])
+    {
+        if(!vis[v])
+        {   
+            if(isCyclicConn(adj,vis,v,s))
+            return true;
+        }
+        
+        else if(vis[v]==true && v!=parent)
+        return true;
+    }
+    
+    return false;
+}
+
+bool isCyclic(vector<int> adj[],int V)
+{
+    vector<bool> vis(V,false);
+    
+    for(int i=0;i<V;i++)
+    {
+        if(!vis[i])
+        {
+            if(isCyclicConn(adj,vis,i,-1))
+            return true;
+        }
+    }
+    return false;
+}
