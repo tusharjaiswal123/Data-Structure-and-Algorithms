4e89cb9ec3b34761b1799a2faec4bd5feb11469b
Tushar Jaiswal <54629298+tusharjaiswal123@users.noreply.github.com>
2020-03-19 14:05:51 +0530

    Update DFS.cpp

--- DFS.cpp
+++ DFS.cpp
@@ -11,3 +11,48 @@ void dfs(int s, vector<int> g[], bool vis[])
     }
 
 }
+
+int main()
+{
+    //call bfs for all unvisited vetex in case if graph is disconnected
+}
+
+
+## Iterative DFS
+
+void itrdfs(vector<int> adj[],vector<int>& vis,int src)
+{
+     stack<int> s;
+     s.push(src);
+    
+    while(!s.empty())
+    {
+        int k=s.top();
+        s.pop();
+        
+        if(vis[k])
+            continue;
+        
+        vis[k]=true;
+        cout<<k<<" ";
+        
+        for(auto j:adj[k])
+        {   
+            if(!vis[j]
+               s.push(j);
+        }
+    }
+}
+
+int main()
+{
+    vector<int> adj[V];
+    vector<int> vis(V,false);
+    
+    for(i=0;i<V;i++)
+    {
+        itrdfs(adj,vis,i)
+    }
+    
+    return 0;
+}
