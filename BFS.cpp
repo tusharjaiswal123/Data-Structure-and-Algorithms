641dc88ca283e9f2e055a4017641da6a6aa325c7
Tushar Jaiswal <54629298+tusharjaiswal123@users.noreply.github.com>
2020-03-19 13:55:07 +0530

    Update BFS.cpp

--- BFS.cpp
+++ BFS.cpp
@@ -22,3 +22,42 @@ void bfs(int s,vector<int> adj[], bool vis[])
     }
     
 }
+
+
+## Recursive BFS
+
+int main()
+{   
+    vector<int> vis(V,false);
+    queue<int> q;
+    q.push(0);  // q.push(s);  s is source
+    vis[0]=true;  // vis[s]=true;
+    
+    for(i=0;i<v;i++)     //for all unconnected components of graph
+    {   
+        if(!vis[i])
+        bfs(adj,vis,q);
+    }    
+}
+
+void bfs(vector<int> adj[], vector<int>& vis,queue<int>& q)
+{
+    if(q.empty())
+        return;
+
+    int u=q.front();
+    q.pop();
+    cout<<u<<" ";
+        
+    for(int v:adj[u])
+    {
+         if(vis[v]==false)
+         {
+             q.push(v);
+             vis[v]=true;
+         }
+    }
+    
+    bfs(adj,vis,q);
+}
+
