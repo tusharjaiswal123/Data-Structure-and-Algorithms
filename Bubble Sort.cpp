a613d576a0eb0e382d851cae6696a83c0fd82cca
Tushar Jaiswal <54629298+tusharjaiswal123@users.noreply.github.com>
2020-03-27 13:59:43 +0530

    Update Bubble Sort.cpp

--- Bubble Sort.cpp
+++ Bubble Sort.cpp
@@ -1,6 +1,6 @@
 void bubbleSort(int arr[], int n)    //Swap adjacent elements if a[j+1] > a[j]   
 {   int i,j;                         //Repeat this n times  O(n^2)
-
+                                     //We can optimize bubble sort so best case time complexity O(n)
     for(i=0;i<n-1;i++)
     {
       for(j=0;j<n-i-1;j++)
