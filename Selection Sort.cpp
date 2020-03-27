8b4662788c5dc0480248249833cbdb7e2c788194
Tushar Jaiswal <54629298+tusharjaiswal123@users.noreply.github.com>
2020-03-27 14:13:46 +0530

    Create Selection Sort.cpp

---
+++ Selection Sort.cpp
@@ -0,0 +1,19 @@
+void selectsort(int arr[], int n)
+{
+    int i,j;                      // select maximum element and swap it wiht last position element
+                                  //Time complexity O(n^2) in all cases
+    for(i=n-1;i>=0;i++)
+    {   
+        int mx=-1,ans;
+        for(j=0;j<=i;j++)
+        {
+          if(arr[j]>mx)
+          {
+             mx=arr[j];
+             ans=j;
+          }
+          swap(&a[i],&a[ans]);
+        }
+    }    
+    
+}
