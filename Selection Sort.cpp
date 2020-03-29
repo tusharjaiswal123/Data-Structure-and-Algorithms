9c4cc6e208e7861e05ea212590522b5b5d3634ff
Tushar Jaiswal <54629298+tusharjaiswal123@users.noreply.github.com>
2020-03-29 13:44:12 +0530

    Update Selection Sort.cpp

--- Selection Sort.cpp
+++ Selection Sort.cpp
@@ -1,6 +1,6 @@
 void selectsort(int arr[], int n)
 {
-    int i,j;                      // select maximum element and swap it wiht last position element
+    int i,j;                      // select maximum element and swap it with last position element
                                   //Time complexity O(n^2) in all cases
     for(i=n-1;i>=0;i++)
     {   
