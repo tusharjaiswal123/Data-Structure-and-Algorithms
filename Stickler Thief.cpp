9a965f54d779a317923b21c08f174396784e4c2a
Tushar Jaiswal <54629298+tusharjaiswal123@users.noreply.github.com>
2020-04-08 14:05:51 +0530

    Update Stickler Thief.cpp

--- Stickler Thief.cpp
+++ Stickler Thief.cpp
@@ -1,8 +1,13 @@
 PROBLEM:
 
-Stickler the thief wants to loot money from a society having n houses in a single line. He is a weird person and follows a certain rule when looting the houses. According to the rule, he will never loot two consecutive houses. At the same time, he wants to maximize the amount he loots. The thief knows which house has what amount of money but is unable to come up with an optimal looting strategy. He asks for your help to find the maximum money he can get if he strictly follows the rule. Each house has a[i] amount of money present in it.
+Stickler the thief wants to loot money from a society having n houses in a single line. He is a weird person and follows a certain rule
+when looting the houses. According to the rule, he will never loot two consecutive houses. At the same time, he wants to maximize the
+amount he loots. The thief knows which house has what amount of money but is unable to come up with an optimal looting strategy. He
+asks for your help to find the maximum money he can get if he strictly follows the rule. Each house has a[i] amount of money present
+in it.
 Input:
-The first line of input contains an integer T denoting the number of test cases. T testcases follow. Each test case contains an integer n which denotes the number of houses. Next line contains space separated numbers denoting the amount of money in each house.
+The first line of input contains an integer T denoting the number of test cases. T testcases follow. Each test case contains an integer
+n which denotes the number of houses. Next line contains space separated numbers denoting the amount of money in each house.
 Output:
 For each testcase, in a newline, print an integer which denotes the maximum amount he can take home.
 Constraints:
