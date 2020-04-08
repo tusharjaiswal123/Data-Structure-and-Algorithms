8f0530652130802643a8941f1175f346b5df1606
Tushar Jaiswal <54629298+tusharjaiswal123@users.noreply.github.com>
2020-04-08 13:38:45 +0530

    Update Nth Catalan Number.cpp

--- Nth Catalan Number.cpp
+++ Nth Catalan Number.cpp
@@ -3,7 +3,8 @@ PROBLEM:
 Given a number N. The task is to find the nth catalan number.
 The first few Catalan numbers for n = 0, 1, 2, 3, … are 1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862, …
 Input:
-First line of input contains a single integer T which denotes the number of test cases. First line of each test case contains a single integer N.
+First line of input contains a single integer T which denotes the number of test cases. First line of each test case contains a single
+integer N.
 Output:
 For each test case, in a new line print the catalan number at position N.
 Note: Positions start from 0 as shown above.
