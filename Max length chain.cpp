f23552f9c423198ec18b1ab8a9b18dbf9d74403d
Tushar Jaiswal <54629298+tusharjaiswal123@users.noreply.github.com>
2020-04-08 14:09:54 +0530

    Update Max length chain.cpp

--- Max length chain.cpp
+++ Max length chain.cpp
@@ -1,9 +1,12 @@
 PROBLEM:
 
-You are given N pairs of numbers. In every pair, the first number is always smaller than the second number. A pair (c, d) can follow another pair (a, b) if b < c. Chain of pairs can be formed in this fashion. Your task is to complete the function maxChainLen which returns an integer denoting the longest chain which can be formed from a given set of pairs. 
+You are given N pairs of numbers. In every pair, the first number is always smaller than the second number. A pair (c, d) can follow
+another pair (a, b) if b < c. Chain of pairs can be formed in this fashion. Your task is to complete the function maxChainLen which
+returns an integer denoting the longest chain which can be formed from a given set of pairs. 
 
 Input:
-The first line of input contains an integer T denoting the no of test cases then T test cases follow .Then T test cases follow . The first line of input contains an integer N denoting the no of pairs . In the next line are 2*N space separated values denoting N pairs.
+The first line of input contains an integer T denoting the no of test cases then T test cases follow .Then T test cases follow . The
+first line of input contains an integer N denoting the no of pairs . In the next line are 2*N space separated values denoting N pairs.
 
 Output:
 For each test case output will be the length of the longest chain formed.
