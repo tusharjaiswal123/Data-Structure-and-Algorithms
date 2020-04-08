4cd023802a089f241939b8a9d641a78ad2a4545c
Tushar Jaiswal <54629298+tusharjaiswal123@users.noreply.github.com>
2020-04-08 19:48:00 +0530

    Create K-Palindrome.cpp

---
+++ K-Palindrome.cpp
@@ -0,0 +1,51 @@
+PROBLEM:
+
+A string is k palindrome if it can be transformed into a palindrome on removing at most k characters from it. Your task is to complete
+the function is_k_palin which takes two arguments a string str and a number N . Your function should return true if the string is k
+palindrome else it should return false.
+
+Input:
+The first line of input is an integer T denoting the number of test cases . Then T test cases follow . Each test case  contains a
+string str and an integer N separated by space.  
+
+Output:
+The output will be 1 if the string is  k palindrome else 0 .
+
+Constraints:
+1<=T<=100
+1<=length of str<=100
+1<=N<=20
+
+Example:
+Input
+2
+abcdecba 1
+acdcb  1
+Output
+1
+0
+
+SOLUTION:
+
+A string is k palindrome if it can be transformed into a palindrome on removing at most k characters from it. Your task is to complete the function is_k_palin which takes two arguments a string str and a number N . Your function should return true if the string is k palindrome else it should return false.
+
+Input:
+The first line of input is an integer T denoting the number of test cases . Then T test cases follow . Each test case  contains a string str and an integer N separated by space.  
+
+Output:
+The output will be 1 if the string is  k palindrome else 0 .
+
+Constraints:
+1<=T<=100
+1<=length of str<=100
+1<=N<=20
+
+Example:
+Input
+2
+abcdecba 1
+acdcb  1
+Output
+1
+0
+
