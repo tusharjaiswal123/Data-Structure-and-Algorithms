80cb4892eda7b0b374af0f8899506932a09c39b6
Tushar Jaiswal <54629298+tusharjaiswal123@users.noreply.github.com>
2020-04-08 13:42:00 +0530

    Update Gold Mine Problem.cpp

--- Gold Mine Problem.cpp
+++ Gold Mine Problem.cpp
@@ -1,6 +1,9 @@
 PROBLEM:
 
-Given a gold mine (M) of n*m dimensions. Each field in this mine contains a positive integer which is the amount of gold in tons. Initially the miner is at first column but can be at any row. He can move only (right->,right up /,right down\) that is from a given cell, the miner can move to the cell diagonally up towards the right or right or diagonally down towards the right. Your task is to find out maximum amount of gold which he can collect.
+Given a gold mine (M) of n*m dimensions. Each field in this mine contains a positive integer which is the amount of gold in tons.
+Initially the miner is at first column but can be at any row. He can move only (right->,right up /,right down\) that is from a
+given cell, the miner can move to the cell diagonally up towards the right or right or diagonally down towards the right. Your
+task is to find out maximum amount of gold which he can collect.
 Examples:
 Input : M[][] = {{1, 3, 3},
                  {2, 1, 4},
@@ -18,7 +21,8 @@ Output : 16
 
 
 Input:
-The first line of input contains an integer T denoting the no of test cases. Then T test cases follow. Each test case contains an integer n and m denoting the size of the matrix. Then in the next line are n*m space separated values of the matrix.
+The first line of input contains an integer T denoting the no of test cases. Then T test cases follow. Each test case contains an integer
+n and m denoting the size of the matrix. Then in the next line are n*m space separated values of the matrix.
 
 Output:
 For each test case in a new line print an integer denoting the max gold the miner could collect.
