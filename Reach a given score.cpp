5789a3b8fd2a434c7452b210b090208416d03186
Tushar Jaiswal <54629298+tusharjaiswal123@users.noreply.github.com>
2020-05-04 13:03:11 +0530

    Update Reach a given score.cpp

--- Reach a given score.cpp
+++ Reach a given score.cpp
@@ -1,8 +1,10 @@
 PROBLEM:
 
-Consider a game where a player can score 3 or 5 or 10 points in a move. Given a total score n, find number of distinct combinations to reach the given score.
+Consider a game where a player can score 3 or 5 or 10 points in a move. Given a total score n, find number of distinct combinations to
+reach the given score.
 Input:
-The first line of input contains an integer T denoting the number of test cases. T testcases follow.The first line of each test case is n.
+The first line of input contains an integer T denoting the number of test cases. T testcases follow.The first line of each test
+case is n.
 Output:
 For each testcase, in a new line, print number of ways/combinations to reach the given score.
 Constraints:
