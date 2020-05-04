b9fa652dc54be464905e31a6ca514bca2c0db3d4
Tushar Jaiswal <54629298+tusharjaiswal123@users.noreply.github.com>
2020-05-04 13:54:22 +0530

    Update Minimum Number of Coins.cpp

--- Minimum Number of Coins.cpp
+++ Minimum Number of Coins.cpp
@@ -1,8 +1,10 @@
 PROBLEM:
 
-Given a value V. You have to make change for V cents, given that you have infinite supply of each of C{ C1, C2, .. , Cm} valued coins. Find the minimum number of coins to make the change.
+Given a value V. You have to make change for V cents, given that you have infinite supply of each of C{ C1, C2, .. , Cm} valued coins.
+Find the minimum number of coins to make the change.
 Input:
-The first line of input contains an integer T denoting the number of test cases. The first line of each test case is V and N, V is the value of cents and N is the number of coins. The second line of each test case contains N input C[i], value of available coins.
+The first line of input contains an integer T denoting the number of test cases. The first line of each test case is V and N, V is the
+value of cents and N is the number of coins. The second line of each test case contains N input C[i], value of available coins.
 Output:
 Print the minimum number of coins to make the change, if not possible print "-1".
 Constraints:
