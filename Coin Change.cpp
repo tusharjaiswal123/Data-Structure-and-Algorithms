144c1cc38a60a1d8b4d11dcc9d7ce57e0b3fe04f
Tushar Jaiswal <54629298+tusharjaiswal123@users.noreply.github.com>
2020-05-04 13:51:32 +0530

    Update Coin Change.cpp

--- Coin Change.cpp
+++ Coin Change.cpp
@@ -1,8 +1,13 @@
 PROBLEM:
 
-Given a value N, find the number of ways to make change for N cents, if we have infinite supply of each of S = { S1, S2, .. , Sm} valued coins. The order of coins doesn’t matter. For example, for N = 4 and S = {1,2,3}, there are four solutions: {1,1,1,1},{1,1,2},{2,2},{1,3}. So output should be 4. For N = 10 and S = {2, 5, 3, 6}, there are five solutions: {2,2,2,2,2}, {2,2,3,3}, {2,2,6}, {2,3,5} and {5,5}. So the output should be 5.
+Given a value N, find the number of ways to make change for N cents, if we have infinite supply of each of S = { S1, S2, .. , Sm} valued
+coins. The order of coins doesn’t matter. For example, for N = 4 and S = {1,2,3}, there are four solutions:
+{1,1,1,1},{1,1,2},{2,2},{1,3}. So output should be 4. For N = 10 and S = {2, 5, 3, 6}, there are five solutions:
+{2,2,2,2,2}, {2,2,3,3}, {2,2,6}, {2,3,5} and {5,5}. So the output should be 5.
 Input: 
-The first line contains an integer 'T' denoting the total number of test cases. In each test cases, the first line contains an integer 'M' denoting the size of array. The second line contains M space-separated integers A1, A2, ..., AN denoting the elements of the array. The third line contains an integer 'N' denoting the cents.
+The first line contains an integer 'T' denoting the total number of test cases. In each test cases, the first line contains an integer
+'M' denoting the size of array. The second line contains M space-separated integers A1, A2, ..., AN denoting the elements of the array.
+The third line contains an integer 'N' denoting the cents.
 Output:
 Print number of possible ways to make change for N cents.
 Constraints:
