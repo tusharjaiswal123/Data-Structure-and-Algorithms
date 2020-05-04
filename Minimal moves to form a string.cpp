510b4c80bc0d20ec3039729295418c46ec5a2eb8
Tushar Jaiswal <54629298+tusharjaiswal123@users.noreply.github.com>
2020-05-04 13:01:08 +0530

    Update Minimal moves to form a string.cpp

--- Minimal moves to form a string.cpp
+++ Minimal moves to form a string.cpp
@@ -1,6 +1,7 @@
 PROBLEM:
 
-Given a string S, we need to write a program to check if it is possible to construct the given string S by performing any of the below operations any number of times. In each step, we can:
+Given a string S, we need to write a program to check if it is possible to construct the given string S by performing any of the
+below operations any number of times. In each step, we can:
 Add any character at the end of the string.
 or, append the string to the string itself.
 The above steps can be applied any number of times. We need to print the minimum steps required to form the string.
