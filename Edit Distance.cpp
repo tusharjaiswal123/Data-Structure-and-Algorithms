3dd40cb6705e85759c9b817fec1f65440ffe1748
Tushar Jaiswal <54629298+tusharjaiswal123@users.noreply.github.com>
2020-05-04 13:32:50 +0530

    Update Edit Distance.cpp

--- Edit Distance.cpp
+++ Edit Distance.cpp
@@ -1,15 +1,18 @@
 PROBLEM:
 
-Given two strings str1 and str2 and below operations that can performed on str1. Find minimum number of edits (operations) required to convert ‘str1′ into ‘str2′.
+Given two strings str1 and str2 and below operations that can performed on str1. Find minimum number of edits (operations) required to
+convert ‘str1′ into ‘str2′.
 Insert
 Remove
 Replace
 All of the above operations are of cost=1.
 Both the strings are of lowercase.
 Input:
-The First line of the input contains an integer T denoting the number of test cases. Then T test cases follow. Each tese case consists of two lines. The first line of each test case consists of two space separated integers P and Q denoting the length of the strings str1 and str2 respectively. The second line of each test case coantains two space separated strings str1 and str2 in order.
+The First line of the input contains an integer T denoting the number of test cases. Then T test cases follow. Each tese case consists
+of two lines. The first line of each test case consists of two space separated integers P and Q denoting the length of the strings str1
+and str2 respectively. The second line of each test case coantains two space separated strings str1 and str2 in order.
 Output:
-Corresponding to each test case, pirnt in a new line, the minimum number of operations required.
+Corresponding to each test case, print in a new line, the minimum number of operations required.
 Constraints:
 1 <= T <= 50
 1 <= P <= 100
