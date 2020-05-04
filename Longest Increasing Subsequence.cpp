4e1298d647ff135e00979e26494ef433df9c912b
Tushar Jaiswal <54629298+tusharjaiswal123@users.noreply.github.com>
2020-05-04 13:25:07 +0530

    Update Longest Increasing Subsequence.cpp

--- Longest Increasing Subsequence.cpp
+++ Longest Increasing Subsequence.cpp
@@ -1,7 +1,7 @@
 PROBLEM:
 
 Given a sequence A of size N, find the length of the longest increasing subsequence from a given sequence .
-The longest increasing subsequence means to find a subsequence of a given sequence in which the subsequence's elements are in sorted 
+The longest increasing subsequence means to find a subsequence of a given sequence in which the subsequences elements are in sorted 
 order, lowest to highest, and in which the subsequence is as long as possible. This subsequence is not necessarily contiguous, or unique.
 Note: Duplicate numbers are not counted as increasing subsequence.
 Input:
