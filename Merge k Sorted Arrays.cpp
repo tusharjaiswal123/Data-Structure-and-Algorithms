fc3109b0a4db6b8ced8099a5e52b5c0ccc4e8fb4
Tushar Jaiswal <54629298+tusharjaiswal123@users.noreply.github.com>
2020-06-26 15:12:04 +0530

    Update Merge k Sorted Arrays.cpp

--- Merge k Sorted Arrays.cpp
+++ Merge k Sorted Arrays.cpp
@@ -30,3 +30,54 @@ int *mergeKArrays(int arr[][N], int k)
     
     return a;
 }
+
+
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+	int n,k,i,j;
+	cin>>k>>n;
+
+	int a[k][n];
+
+	priority_queue< pair<int, pair<int,int> >,vector< pair<int, pair<int,int> > >,
+	greater< pair<int, pair<int,int> > > > pq;
+	vector<int> ans;
+
+	for(i=0;i<k;i++)
+	{
+		for(j=0;j<n;j++)
+		cin>>a[i][j];
+	}
+
+	for(i=0;i<k;i++)
+	{	
+		pq.push({a[i][0],{i,0}});
+	}
+
+	while(!pq.empty())
+	{
+		auto p=pq.top();
+		pq.pop();
+
+		ans.push_back(p.first);
+
+		if(p.second.second+1<n)
+		{
+			pq.push({a[p.second.first][p.second.second+1],{p.second.first,p.second.second+1}});
+		}
+	}
+
+	for(auto s:ans)
+	cout<<s<<" ";
+
+	cout<<endl;
+
+	return 0;
+}
