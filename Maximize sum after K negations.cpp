1e7ff717f154195caf589f50fd7b6ea31ccab2eb
Tushar Jaiswal <54629298+tusharjaiswal123@users.noreply.github.com>
2020-08-12 16:20:59 +0530

    Update Maximize sum after K negations.cpp

--- Maximize sum after K negations.cpp
+++ Maximize sum after K negations.cpp
@@ -1,8 +1,10 @@
 PROBLEM:
 
-Given an array of integers of size N and a number K. Your must modify array K number of times. Here modify array means in each operation you can replace any array element either arr[i] by -arr[i] or -arr[i] by arr[i]. You need to perform this operation in such a way that after K operations, sum of array must be maximum.
+Given an array of integers of size N and a number K. Your must modify array K number of times. Here modify array means in each operation you can replace any array element either
+arr[i] by -arr[i] or -arr[i] by arr[i]. You need to perform this operation in such a way that after K operations, sum of array must be maximum.
 Input:
-The first line of input contains an integer T denoting the number of test cases. Then T test cases follow. Each test case consist of two lines . The first line of each test case contains an integer N and K. The second line of each test case contains N space separated integers denoting elements of the array.
+The first line of input contains an integer T denoting the number of test cases. Then T test cases follow. Each test case consist of two lines . The first line of each test
+case contains an integer N and K. The second line of each test case contains N space separated integers denoting elements of the array.
 Output:
 For each test case, print maximum possible sum.
 Constraints:
