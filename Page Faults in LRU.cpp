81cca3c95ee46e6f4270cfed0165a6a99e81a327
Tushar Jaiswal <54629298+tusharjaiswal123@users.noreply.github.com>
2020-08-12 16:25:01 +0530

    Update Page Faults in LRU.cpp

--- Page Faults in LRU.cpp
+++ Page Faults in LRU.cpp
@@ -1,8 +1,11 @@
 PROBLEM:
 
-In operating systems that use paging for memory management, page replacement algorithm are needed to decide which page needs to be replaced when the new page comes in. Whenever a new page is referred and is not present in memory, the page fault occurs and Operating System replaces one of the existing pages with a newly needed page. Given a sequence of pages and memory capacity, your task is to find the number of page faults using Least Recently Used (LRU) Algorithm.
+In operating systems that use paging for memory management, page replacement algorithm are needed to decide which page needs to be replaced when the new page comes in.
+Whenever a new page is referred and is not present in memory, the page fault occurs and Operating System replaces one of the existing pages with a newly needed page.
+Given a sequence of pages and memory capacity, your task is to find the number of page faults using Least Recently Used (LRU) Algorithm.
 Input:
-The first line of input contains an integer T denoting the number of test cases. Each test case contains n number of pages and next line contains space seaprated sequence of pages. The following line consist of the capacity of the memory. 
+The first line of input contains an integer T denoting the number of test cases. Each test case contains n number of pages and next line contains space seaprated sequence
+of pages. The following line consist of the capacity of the memory. 
 Note: Pages are referred in the order left to right from the array (i.e index 0 page is referred first then index 1 and so on). Memory is empty at the start.
 Output:
 Output the number of page faults.
