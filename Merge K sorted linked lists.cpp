6ae7db1d52ab9251cece8fe381c2b004979d8608
Tushar Jaiswal <54629298+tusharjaiswal123@users.noreply.github.com>
2021-01-24 13:58:19 +0530

    Update Merge K sorted linked lists.cpp

--- Merge K sorted linked lists.cpp
+++ Merge K sorted linked lists.cpp
@@ -36,3 +36,140 @@ Node * mergeKLists(Node *arr[], int N)
     
     return head;
 }
+
+
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        
+        int i,n;
+        n = lists.size();
+        ListNode *head=NULL,*p;
+        
+        if(n==0)
+            return head;
+        
+        priority_queue< pair<int,ListNode*>, vector<pair<int,ListNode*>>, greater<pair<int,ListNode*>> > pq;
+        
+        for(i=0;i<n;i++)
+        {   
+            if(lists[i]!=NULL)
+            {
+                pq.push({lists[i]->val,lists[i]});
+            }
+        }
+        
+        
+        while(!pq.empty())
+        {
+            auto k=pq.top();
+            pq.pop();
+
+            
+            if(head==NULL)
+            {
+                head=k.second;
+                p=head;
+            }
+            else
+            {
+                p->next=k.second;
+                p=p->next;
+            }
+            
+            if((k.second)->next!=NULL)
+            {
+                pq.push({(k.second->next)->val,k.second->next});
+            }
+            
+        }
+        
+        
+        return head;
+    }
+};
+
+
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    struct cmp{
+        bool operator()(const ListNode *a,const ListNode *b)
+        {
+            return a->val > b->val;
+        }
+    };
+    
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        
+        int i,n;
+        n = lists.size();
+        ListNode *head=NULL,*p;
+        
+        if(n==0)
+            return head;
+        
+        priority_queue<ListNode*, vector<ListNode*>, cmp > pq;
+        
+        for(i=0;i<n;i++)
+        {   
+            if(lists[i]!=NULL)
+            {
+                pq.push(lists[i]);
+            }
+        }
+        
+        
+        while(!pq.empty())
+        {
+            auto k=pq.top();
+            pq.pop();
+
+            
+            if(head==NULL)
+            {
+                head=k;
+                p=head;
+            }
+            else
+            {
+                p->next=k;
+                p=p->next;
+            }
+            
+            if(k->next!=NULL)
+            {
+                pq.push(k->next);
+            }
+            
+        }
+        
+        
+        return head;
+    }
+};
