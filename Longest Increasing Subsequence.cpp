24b63a1399dbd9930c7510ef346b824237c7bbb0
Tushar Jaiswal <54629298+tusharjaiswal123@users.noreply.github.com>
2021-07-09 15:15:17 +0530

    Update Longest Increasing Subsequence.cpp

--- Longest Increasing Subsequence.cpp
+++ Longest Increasing Subsequence.cpp
@@ -99,3 +99,24 @@ int main()
 
     return 0;
 }
+
+
+int lengthOfLIS(vector<int>& nums) {
+        
+        int n,i;
+        n = nums.size();
+        
+        vector<int> res;
+        
+        for(i=0;i<n;i++)
+        {
+            auto it = lower_bound(res.begin(),res.end(),nums[i]);
+            
+            if(it == res.end())
+                res.push_back(nums[i]);
+            else
+                *it = nums[i];
+        }
+        
+        return res.size();
+    }
